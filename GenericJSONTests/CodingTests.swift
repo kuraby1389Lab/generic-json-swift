281604fd2b0a293e01da78a9d62c0a37234dc3ee
Trystan <trystan.p@m3dicine.com>
2023-01-13 11:51:33 +1000

    Update to coding tests to use JSON(from: String) initialisation method which avoids forced unwrapping during execution

--- GenericJSONTests/CodingTests.swift
+++ GenericJSONTests/CodingTests.swift
@@ -34,7 +34,7 @@ class CodingTests: XCTestCase {
         let input = """
             {"array":[1],"num":1,"bool":true,"obj":{},"null":null,"str":"baz"}
             """
-        let json = try! JSONDecoder().decode(JSON.self, from: input.data(using: .utf8)!)
+        let json = try! JSON(from: input)
         XCTAssertEqual(json, [
             "num": 1,
             "str": "baz",
@@ -46,13 +46,14 @@ class CodingTests: XCTestCase {
     }
 
     func testDecodingBool() throws {
-        XCTAssertEqual(try JSONDecoder().decode(JSON.self, from: "{\"b\":true}".data(using: .utf8)!), ["b":true])
-        XCTAssertEqual(try JSONDecoder().decode(JSON.self, from: "{\"n\":1}".data(using: .utf8)!), ["n":1])
+        XCTAssertEqual(try JSON(from: "{\"b\":true}"), ["b":true])
+        XCTAssertEqual(try JSON(from: "{\"b\":true}"), ["b":true])
+        XCTAssertEqual(try JSON(from: "{\"n\":1}"), ["n":1])
     }
 
     func testEmptyCollectionDecoding() throws {
-        XCTAssertEqual(try JSONDecoder().decode(JSON.self, from: "[]".data(using: .utf8)!), [])
-        XCTAssertEqual(try JSONDecoder().decode(JSON.self, from: "{}".data(using: .utf8)!), [:])
+        XCTAssertEqual(try JSON(from: "[]"), [])
+        XCTAssertEqual(try JSON(from: "{}"), [:])
     }
 
     func testDebugDescriptions() {
